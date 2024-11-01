3307c2ae88097e6c46495a98febd93a1a0133101
BANCOARBI\jonas.queiroz <jonas.queiroz@bancoarbi.com.br>
2024-11-01 14:50:23 -0300

    password routes

---
+++ src/routes/password_routes.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, HTTPException, status
+import services.password_services as service
+
+router = APIRouter()
+
+
+@router.get('/validate-password', status_code=status.HTTP_200_OK)
+def validate_password(password: str):
+    if not service.check_length(password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail='Password must have at least 8 characters')
+
+    elif not service.check_uppercase(password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail='Password must have at least one uppercase character')
+
+    elif not service.check_digit(password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail='Password must have at least one digit')
+
+    elif not service.check_lowercase(password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail='Password must have at least one lowercase character')
+
+    elif not service.check_special_characters(password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail='Password must have at least one special character')
+
+    return {'message': 'Password is valid'}
---
+++ src/services/__init__.py
@@ -0,0 +1,1 @@
+import services.password_services
